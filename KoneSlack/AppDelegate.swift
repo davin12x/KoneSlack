600e8b46988236d59934d6a172cbca1d444750ce
Lalit Bagga <davin12x@gmail.com>
2017-07-14 18:13:13 -0400

    Initial Commit

---
+++ KoneSlack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KoneSlack
+//
+//  Created by Lalit Bagga on 2017-07-14.
+//  Copyright © 2017 Lalit Bagga. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
